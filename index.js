d9155d74c8ebcb879ef5588672686edbb3713217
Stephen Parente <stephen@ingenuitydesign.com>
2015-05-28 16:40:08 -0400

    Made it work as a module

--- index.js
+++ index.js
@@ -4,41 +4,56 @@ var colors = require('colors');
 var _ = require('./tools');
 var Exporter = require('./exporter');
 
-args.extend('firebaseUrl', function() {
-	return this.last();
-});
+function fetch(url, path, cb) {
+	if (!cb) cb = function() {};
+	path = path || './export.csv';
 
-colors.setTheme({
-  input: 'grey',
-  verbose: 'grey',
-  error: 'blue',
-  trace: 'red'
-});
+	var firebase = new Firebase(firebaseUrl);
 
-process.on('uncaughtException', function(error) {
-	console.log('Please correct the following error(s)'.underline)
-	if (!error.hasOwnProperty('type')) {
-		console.log( error.stack );
-	} else {
-		console.log( error.message.error.bold);
-	}
-});
+	firebase.on('value', function(snap) {
+		var value = snap.val();
+		var array = _.$toArray(value);
+		var data = _.prepareCSV(array);
 
-var firebaseUrl = args.getArgument('firebaseUrl', new Error('Must specify a firebase URL'));
+		var e = new Exporter(path);
+		e.writeToFile(data);
 
-var firebase = new Firebase(firebaseUrl);
+		e.on('finished', cb);
+		e.on('error', function(err) {
+			throw err;
+		});
 
-firebase.on('value', function(snap) {
-	var value = snap.val();
-	var array = _.$toArray(value);
-	var data = _.prepareCSV(array);
+	});
+}
 
-	var e = new Exporter('./export.csv');
-	e.writeToFile(data);
+if (require.main === module) {
 
-	e.on('finished', process.exit);
-	e.on('error', function(err) {
-		throw err;
+	args.extend('firebaseUrl', function() {
+		return this.last();
 	});
 
-});
+	var firebaseUrl = args.getArgument('firebaseUrl', new Error('Must specify a firebase URL'));
+
+	colors.setTheme({
+	  input: 'grey',
+	  verbose: 'grey',
+	  error: 'blue',
+	  trace: 'red'
+	});
+
+	process.on('uncaughtException', function(error) {
+		console.log('Please correct the following error(s)'.underline)
+		if (!error.hasOwnProperty('type')) {
+			console.log( error.stack );
+		} else {
+			console.log( error.message.error.bold);
+		}
+	});
+
+	fetch(firebaseUrl, false, function() {
+		process.exit();
+	});
+
+}
+
+module.exports = fetch;
